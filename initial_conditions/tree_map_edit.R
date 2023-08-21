336b49ac2a861108fc3618de63c9d1f021136168
Maxence Soubeyrand <106389726+MaxenceSoubeyrand@users.noreply.github.com>
2023-08-21 08:28:49 -0400

    Update tree_map_edit.R
    Remove the resef dataset

--- initial_conditions/tree_map_edit.R
+++ initial_conditions/tree_map_edit.R
@@ -19,46 +19,12 @@ for(i in 1:length(t_files_dir)){
   file.copy(t_files_dir[i], tree_map_name)
 }
 
-#Here I will retrieve temperate hardwood stands from the RESEF data
-con <- odbcConnectAccess2007("initial_conditions/RESEF_30-novembre-2020_UQAT.accdb") #One year embargo
-data_tables <- sqlTables(con ,TABVLE_TYPE=='TABLE')$TABLE_NAME
-dendro <- sqlFetch(con, "t_Dendrometrie")
 
-# Calculation of X-Y coordinates of trees per plot
-# I select for the RHS because there are pure stands and I put the same for the other two temperate hardwoods, the results will then be comparable. 
-dendro2 <- mutate(dendro, PX = PEP %/% 10, PY = PEP %% 10,
-                 X = round(PX*10 + AXEX/100, 2), 
-                 Y = round(PY*10 + AXEY/100, 2)) %>% 
-  select(SERIE, PLACE, PEP, ESS, DHP, X, Y) %>% 
-  group_by(SERIE, PLACE, PEP) %>% 
-  summarize(n=n(),
-            pers=sum(ESS=="ERS")/n,
-            ners=sum(ESS=="ERS")) %>% 
-  arrange(desc(pers))
+#Position of the trees in the central part of the simulation, data come from the RESEF dataset under licence.
+big_patch <- readRDS(file="~/initial_conditions/big_patch.rds")
 
-#I take SERIES B, PLACE 802 and PEP 4 because it is a pure sugar maple stand
 
-patch <- mutate(dendro, PX = PEP %/% 10, PY = PEP %% 10,
-                     X = round(PX*10 + AXEX/100, 2)-90, 
-                     Y = round(PY*10 + AXEY/100, 2)) %>% 
-  filter(SERIE=="A", PLACE=="103", PEP == "90") %>% 
-  mutate(Type=ifelse(DHP<100, "Sapling","Adult"),
-         Height=0, 
-         Species="Sugar_Maple",
-         Diam=DHP/10) %>%
-  dplyr::select(X, Y, Species, Type, Diam, Height)
-
-big_patch <- NULL
-for(xi in 0:3){
-  for(yj in 0:3){
-    big_patch <- rbind(big_patch, mutate(patch, X=X+xi*10, Y=Y+yj*10))
-  }
-}
-  
-#The plot of the resef is selected, we must now create the plots that we will put in the simulations 
-#One of 10m2 and another of 400m2
-
-files <- dir("~/initial_conditions/tree_maps/", full.names=F)
+#Functions to create patch of different size
 
 create_patch <- function(patch, size){
   patch %>% 
@@ -69,6 +35,7 @@ create_patch <- function(patch, size){
            Y=Y+50-18.75)
 }
 
+#remove the patch for control simulations
 remove_patch_control <-  function(control, size){
   control %>% 
     mutate(d=(X-50)^2+(Y - 50)^2) %>% 
@@ -76,11 +43,10 @@ remove_patch_control <-  function(control, size){
     dplyr::select(-d)
 }
 
-create_patch(big_patch, 10)
-
+files <- dir("~/initial_conditions/tree_maps/", full.names=F)
 
 init_patch_size <- c(10, 30, 80, 150, 250, 400)
-for(i in files){ #i=files[5]
+for(i in files){ #i=files[1]
   control <- read.table(paste0("~/initial_conditions/tree_maps/",i), h=T)
   
   for(patch_size in init_patch_size){
@@ -90,21 +56,18 @@ for(i in files){ #i=files[5]
     #ERS
     ers <- bind_rows(control_without_patch, patch)
     name_ers <- str_replace(i, "control_NA", paste0("ERS_", patch_size))
-    write.table(x = ers, file = paste0("/tree_maps/", name_ers), sep="\t", row.names=F, quote=F)
+    write.table(x = ers, file = paste0("~/initial_conditions/tree_maps/", name_ers), sep="\t", row.names=F, quote=F)
     
     #ERR
     err <- ers %>% #We take the ers patch and we make as if they were err to have the same initial conditions. 
       mutate(Species=ifelse(Species=="Sugar_Maple", "Red_Maple",Species))
     name_err <- str_replace(i, "control_NA", paste0("ERR_", patch_size))
-    write.table(x = err, file = paste0("/tree_maps/", name_err), sep="\t", row.names=F, quote=F)
+    write.table(x = err, file = paste0("~/initial_conditions/tree_maps/", name_err), sep="\t", row.names=F, quote=F)
     
     #ERR
     boj <- ers %>% 
       mutate(Species=ifelse(Species=="Sugar_Maple", "Yellow_Birch",Species))
     name_boj <- str_replace(i, "control_NA", paste0("BOJ_", patch_size))
-    write.table(x = boj, file = paste0("/tree_maps/", name_boj), sep="\t", row.names=F, quote=F)
+    write.table(x = boj, file = paste0("~/initial_conditions/tree_maps/", name_boj), sep="\t", row.names=F, quote=F)
   }
 }
-
-
-#once tree maps edited
